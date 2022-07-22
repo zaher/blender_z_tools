291b443afe4e7db74db0b89b0feda1ae59391aa0
Zaher Dirkey <zaherdirkey@yahoo.com>
2022-07-22 14:48:13 +0300

    add export bones

--- rename_bones.py
+++ rename_bones.py
@@ -13,23 +13,17 @@ bl_info = {
     "category": "User"
 }
 
-
 import bpy
 from bpy.types import Operator
 from bpy.props import FloatVectorProperty
 #from bpy_extras.object_utils import AddObjectHelper, object_data_add
 from mathutils import Vector
 
-def rename_bones(self, context):
-    dict = {
-        'J_Bip_C_Spine': 'mPelvis',
-        'J_Bip_C_Chest': 'foo1'
-    }
-
-    for b in context.object.data.bones:
-        if b.name in dict.keys():
-            print("renamed: " + b.name + " -> " + dict[b.name])
-            b.name = dict[b.name]
+def top_menu(self, context):
+    self.layout.menu(
+        "PT_ParentMenu",
+        text=PT_ParentMenu.bl_label
+    )
 
 class PT_ParentMenu(bpy.types.Menu):
     bl_idname = "PT_ParentMenu"
@@ -37,6 +31,8 @@ class PT_ParentMenu(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
+
+##################################
 
 class PT_RenameBones(Operator):
 
@@ -52,17 +48,54 @@ class PT_RenameBones(Operator):
 
     def execute(self, context):
 
-        rename_bones(self, context)
+        dict = {
+            'J_Bip_C_Spine': 'mPelvis',
+            '': ''
+        }
+
+        if hasattr(context.object.data, 'bones'):
+            for b in context.object.data.bones:
+                if b.name in dict.keys():
+                    print("renamed: " + b.name + " -> " + dict[b.name])
+                    b.name = dict[b.name]
 
         return {'FINISHED'}
 
-# Registration
+##################################
 
-def top_menu(self, context):
-    self.layout.menu(
-        "PT_ParentMenu",
-        text=PT_ParentMenu.bl_label
-    )
+class PT_ExportBones(Operator):
+
+    bl_idname = "object.export_bones"
+    bl_label = "Export Bones"
+    bl_options = {'REGISTER', 'UNDO'}
+    filepath = bpy.props.StringProperty(subtype="FILE_PATH")
+    filename = bpy.props.StringProperty()
+    directory = bpy.props.StringProperty(subtype="FILE_PATH")
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.operator("wm.open_mainfile")
+        layout.operator("wm.save_as_mainfile")
+
+    def execute(self, context):
+
+        if hasattr(context.object.data, 'bones'):
+            with open(self.filepath, 'w') as f:
+                for b in context.object.data.bones:
+                   f.write(b.name+"\n\r")
+            return {'FINISHED'}
+
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        self.filename = "bones.txt"
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+##################################
+
+# Registration
 
 def rename_menu(self, context):
     self.layout.operator(
@@ -70,23 +103,31 @@ def rename_menu(self, context):
         text=PT_RenameBones.bl_label
     )
 
-def menu_func(self, context):
+def export_menu(self, context):
     self.layout.operator(
-        PT_RenameBones.bl_idname
+        PT_ExportBones.bl_idname,
+        text=PT_ExportBones.bl_label
     )
 
 def register():
     bpy.utils.register_class(PT_ParentMenu)
     bpy.types.VIEW3D_MT_object.append(top_menu)
+
     bpy.utils.register_class(PT_RenameBones)
     bpy.types.PT_ParentMenu.append(rename_menu)
 
+    bpy.utils.register_class(PT_ExportBones)
+    bpy.types.PT_ParentMenu.append(export_menu)
 
 def unregister():
+    bpy.types.PT_ParentMenu.remove(export_menu)
+    bpy.utils.unregister_class(PT_ExportBones)
+
     bpy.types.PT_ParentMenu.remove(rename_menu)
+    bpy.utils.unregister_class(PT_RenameBones)
+
     bpy.types.VIEW3D_MT_object.remove(top_menu)
     bpy.utils.unregister_class(PT_ParentMenu)
-    bpy.utils.unregister_class(PT_RenameBones)
 
 if __name__ == "__main__":
     register()
